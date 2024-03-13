978d1a8c8c46ec7920b802f66867686779dec79c
Santos <gabrielsantoscamargo8@gmail.com>
2024-03-13 19:44:38 -0300

    exemplo funções

--- java/funcoes/exemplo.java
+++ java/funcoes/exemplo.java
@@ -2,9 +2,11 @@ package funcoes;
 
 import java.util.Scanner;
 
-class funcoes_exe01 {
+class funcoes_exemplo {
     private static void linha(int quantidade, char caractere){
-        
+        //Função com retorno de valor
+        //private static tipo_do_dado_do_retorno func(parâmetros){
+        //}
         for(int i = 0; i <= quantidade; i++){
             System.out.printf("%c", caractere);
         }
