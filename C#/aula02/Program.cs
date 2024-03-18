16c81497cf48ecf1a9333d5ed83665adbc87ef08
Santos <gabrielsantoscamargo8@gmail.com>
2024-03-18 19:24:06 -0300

    Adicionado aulas de C#

---
+++ C#/aula02/Program.cs
@@ -0,0 +1,18 @@
+﻿//Implrementação de listas de objetos
+class Program{
+    static void Main(){
+        List<Estudante> Estudantes = new List<Estudante>();
+        // construir 3 estudantes e adicionar na lista estudante
+        Estudante aluno;
+        for(int i = 0; i < 3; i++){
+            aluno = new Estudante("R"+i, "Nome"+i);
+            Estudantes.Add(aluno);
+
+        }
+
+        foreach(var Estudante in Estudantes){
+            Console.WriteLine(Estudante.Rgm + " " + Estudante.Nome);
+        }
+
+    }
+}
