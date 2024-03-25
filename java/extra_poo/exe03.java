6cd0a8fd3619997e465b17e570b6eff6dd9ccd3e
Santos <gabrielsantoscamargo8@gmail.com>
2024-03-25 18:53:55 -0300

    .

--- java/extra_poo/exe03.java
+++ java/extra_poo/exe03.java
@@ -2,18 +2,62 @@ package extra_poo;
 
 class Caneta{
 
-    String marca;
-    String cor;
-    boolean tampada = true;
-    float carga = 100;
-    boolean escrevendo = false;
+    public String marca;
+    public String cor;
+    public double carga;
+    public boolean tampada;
+    public boolean escrevendo ;
+
+    public Caneta(String marca, String cor){
+        this.setCor(cor);
+        this.setMarca(marca);
+        this.setCarga(100);
+        this.setTampada(true);
+        this.setEscrevendo(false);
+    }
+
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    public String getMarca(){
+        return this.marca;
+    }
+
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+    public String getCor(){
+        return this.cor;
+    }
+
+    public void setCarga(double carga){
+        this.carga = carga;
+    }
+    public double getCarga(){
+        return this.carga;
+    }
+
+    public void setTampada(boolean tampada){
+        this.tampada = tampada;
+    }
+    public boolean getTampada(){
+        return this.tampada;
+    }
+
+    public void setEscrevendo(boolean escrevendo){
+        this.escrevendo= escrevendo;
+    }
+    public boolean getEscrevendo(){
+        return this.escrevendo;
+    }
 
     public void status(){
-        System.out.println("Marca: " + this.marca);
-        System.out.println("Cor tinta: " + this.cor);
-        System.out.println("tampada: " + this.tampada);
-        System.out.println("Carga: " + this.carga);
-        System.out.println("Escrevendo: " + this.escrevendo);
+        System.out.println("Marca: " + this.getMarca());
+        System.out.println("Cor tinta: " + this.getCor());
+        System.out.println("tampada: " + this.getTampada());
+        System.out.println("Carga: " + this.getCarga());
+        System.out.println("Escrevendo: " + this.getEscrevendo());
         System.out.println("\n");
 
     }
@@ -69,23 +113,8 @@ class extra_poo_exe03 {
     
     public static void main(String[] args){
 
-        Caneta c1 = new Caneta();
-        c1.marca = "Bic";
-        c1.cor = "Azul";
-    
-        c1.destanpar();
-        c1.escrever();
-        c1.escrever();
-        c1.escrever();
-        c1.escrever();
-        c1.escrever();
-        c1.escrever();
-        c1.escrever();
-        c1.escrever();
-        c1.escrever();
-        c1.escrever();
-        c1.escrever();
-        c1.escrever();
+        Caneta c1 = new Caneta("Bic", "Azul");
+
         c1.status();
 
     }
