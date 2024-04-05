e4b7e5b6da185d4efc6a7108b263cbb1eaea6a13
Gabriel Santos <gabrielsantoscamargo8@gmail.com>
2024-04-05 19:59:36 -0300

    alterações dentro do sistema

--- java/associacao_entre_classes/exe.java
+++ java/associacao_entre_classes/exe.java
@@ -1,5 +1,62 @@
 package associacao_entre_classes;
 
-public class exe {
-    
+import java.util.Scanner;
+
+class Cliente{
+
+    public String nome;
+    public String email;
+    public String endereco;
+
+    Cliente(String nome, String email, String endereco){
+
+        this.nome = nome;
+        this.email = email;
+        this.endereco = endereco;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+}
+
+
+
+class exeAssociacaoEntreClasses {
+        
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        
+
+
+
+
+        input.close();
+
+    }
+
 }
