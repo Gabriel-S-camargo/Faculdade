04a09623be47ecdf02c6b0e7b80cba59a53837c7
Gabriel Santos <gabrielsantoscamargo8@gmail.com>
2024-05-03 19:06:45 -0300

    debugger

---
+++ java/colecoes/mapas/debugger2.java
@@ -0,0 +1,31 @@
+package colecoes.mapas;
+
+import java.util.Map;
+import java.util.HashMap;
+
+class debugger2 {
+    public boolean isAnagram(String s, String t) {
+        char[] charArray = s.toCharArray();
+
+        if(s == t){
+            return true;
+        }
+
+        Map<Character, Integer> mapT = new HashMap();
+
+        for(char testeS : charArray){
+            if(mapT.containsKey(testeS)){
+                return true;
+            }
+            mapT.put(testeS, 1);
+        }
+
+        return false;
+        
+    }
+    public static void main(String[] args){
+        debugger2 d = new debugger2();
+        
+        System.out.println(d.isAnagram("a", "a"));
+    }
+}
