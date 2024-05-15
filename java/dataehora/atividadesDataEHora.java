de00609a107dae02a57e3ddc6c3c41adc06152ef
Gabriel Santos <gabrielsantoscamargo8@gmail.com>
2024-05-15 10:07:51 -0300

    Finalizado atividade de sistema de estacionamento

--- java/dataehora/atividadesDataEHora.java
+++ java/dataehora/atividadesDataEHora.java
@@ -1,25 +1,28 @@
 package dataehora;
 
-
 import java.time.format.DateTimeFormatter;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.Duration;
+import java.util.List;
+import java.util.ArrayList;
 
-class Carro{
+class Carro {
     public String placa;
     public LocalTime horaEntrada;
     public LocalTime horaSaida;
     public double totalAPagar;
 
-    public Carro(String placa){
+    public Carro(String placa) {
         this.placa = placa;
-        this.horaEntrada = LocalTime.now(ZoneId.of("America/Sao_Paulo")); 
+        this.horaEntrada = LocalTime.now(ZoneId.of("America/Sao_Paulo"));
     }
+
+    // Formatadores de Saida
 
     DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-    DateTimeFormatter formatoMinutos = DateTimeFormatter.ofPattern("mm");
+    // Fim Formatadores de Saida
 
     public String getPlaca() {
         return placa;
@@ -44,44 +47,71 @@ class Carro{
     public void setHoraSaida(LocalTime horaSaida) {
         this.horaSaida = horaSaida;
     }
-    
 
-    public void registrarSaida(){
-        this.horaSaida = LocalTime.now();
-        Duration tempoPermanencia = Duration.between(horaEntrada, horaSaida);
-        if(tempoPermanencia.toMinutes() / 60 <= 1){
-            this.totalAPagar = 5.00;
-        }else{
+    public double getTotalAPagar() {
+        return totalAPagar;
+    }
+
+    public void setTotalAPagar(double totalAPagar) {
+        this.totalAPagar = totalAPagar;
+    }
+
+    public void registrarSaida(String horaSaida) {
+
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm");
+
+        LocalTime hora = LocalTime.parse(horaSaida, formato);
+
+        setHoraSaida(hora);
+
+        Duration tempoPermanencia = Duration.between(horaEntrada, hora);
+        if ((tempoPermanencia.toMinutes() - 60 ) <= 1) {
+             setTotalAPagar(5);
+             System.out.println(tempoPermanencia.toMinutes() - 10);
+        } else {
             double tempVar = tempoPermanencia.toMinutes();
 
             tempVar -= 60;
 
             tempVar /= 15;
 
-            this.totalAPagar = (tempVar * 2) + 5;
+            setTotalAPagar((tempVar * 2) + 5);
         }
         System.out.println("Saida registrada!");
     }
 
-    public void imprmirRecibo(){
-        Duration tempoPermanencia = Duration.between(horaEntrada, horaSaida);
-        System.out.println("-----------------------------\r\n" + //
-                        "           Recibo\r\n" + //
-                        "-----------------------------");
-        System.out.println("PLACA: " + getPlaca());
-        System.out.println("Hora entrada: " + getHoraEntrada().format(formato));
-        System.out.println("Hora Saida: " + getHoraSaida().format(formatoMinutos));
-        System.out.println("Tempo de Permanencia: " + tempoPermanencia.toMinutes());
-        System.out.println("Total a pagar: "  );
+    public void imprimirRecibo() {
+
+        if (getHoraSaida() != null) {
+            Duration tempoPermanencia = Duration.between(horaEntrada, horaSaida);
+            System.out.println("-----------------------------\r\n" + //
+                    "           Recibo\r\n" + //
+                    "-----------------------------");
+            System.out.println("PLACA: " + getPlaca());
+            System.out.println("Hora entrada: " + getHoraEntrada().format(formato));
+            System.out.println("Hora Saida: " + getHoraSaida().format(formato));
+            System.out.println("Tempo de Permanencia: " + tempoPermanencia.toMinutes());
+            System.out.printf("Total a pagar: " + "%.2f",getTotalAPagar());
+
+        } else {
+            System.out.println("Impossivel Imprimir Recibo, Saida não registrada");
+        }
     }
-    
 
 }
 
 class atividadesDataEHora {
-    public static void main(String[] args){
-        System.out.println("-----------------------------\r\n" + //
-                        "           Recibo\r\n" + //
-                        "-----------------------------");
+    public static void main(String[] args) {
+
+        List<Carro> estacionamento = new ArrayList<Carro>();
+
+        Carro carro01 = new Carro("AAAA-1234");
+
+        estacionamento.add(carro01);
+
+        estacionamento.get(0).registrarSaida("11:30");
+
+        estacionamento.get(0).imprimirRecibo();
+
     }
 }
