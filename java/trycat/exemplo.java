17560469c55849743e87ca9b838e79c7ce9a0aae
Gabriel Santos <gabrielsantoscamargo8@gmail.com>
2024-05-17 20:54:53 -0300

    ajuste de trycat

--- java/trycat/exemplo.java
+++ java/trycat/exemplo.java
@@ -1,32 +1,44 @@
 package trycat;
 
 
-class Financiamento {
+class Financiamento{
     public double valorTotal;
     public double entrada;
     public int parcelas;
-
-    public Financiamento(double valorTotal, double entrada, int parcelas) {
+    
+    public Financiamento(double valorTotal, double entrada, int parcelas){
+        
+        if(entrada < valorTotal * 0.2){
+            throw new RuntimeException("A entrada deve ser de pelo menos 20%");
+        }
+        else if(parcelas < 6){
+            throw new RuntimeException("O numero minimo de parcelas deve ser 6");
+        }
+        
         this.valorTotal = valorTotal;
         this.entrada = entrada;
         this.parcelas = parcelas;
     }
-
-    public double valorPrestacao() {
-        return (this.valorTotal - this.entrada) / this.parcelas;
-    }
-
     
-
+    public double prestacao(){
+        return (valorTotal - entrada) / parcelas;
+    }
 }
 
 class exemplo {
 
-    public static void main(String[]args){
-
-        Financiamento financiamento01 = new Financiamento(1000, 100, 10);
-
-        System.out.println(financiamento01.valorPrestacao());
+    public static void main(String[] args) {
+        double valorTotal = 1000;
+        double entrada = 200;
+        int parcelas = 6;
+        
+        try{
+            Financiamento f = new Financiamento(valorTotal, entrada, parcelas);
+            System.out.println(f.prestacao()); 
+        }
+        catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
     }
     
 }
